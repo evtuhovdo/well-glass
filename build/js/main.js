374c2c4eaa9a1b806d28baf968b98453a0152bea
Denis Evtukhov <evtuhovdo@gmail..com>
2019-10-29 00:54:19 +0700

    Билд и минификация.

--- build/js/main.js
+++ build/js/main.js
@@ -1,15 +1,3 @@
-function handleWindow() {
-  var body = document.querySelector('body');
-
-  if (window.innerWidth > body.clientWidth + 5) {
-    body.classList.add('has-scrollbar');
-    body.setAttribute('style', '--scroll-bar: ' + (window.innerWidth - body.clientWidth) + 'px');
-  } else {
-    body.classList.remove('has-scrollbar');
-  }
-}
-
-handleWindow();
-
-// The resize isn't very necessary:
-window.addEventListener('resize', handleWindow);
+function handleWindow(){var body=document.querySelector('body');if(window.innerWidth>body.clientWidth+5){body.classList.add('has-scrollbar');body.setAttribute('style','--scroll-bar: '+(window.innerWidth-body.clientWidth)+'px')}else{body.classList.remove('has-scrollbar')}}
+handleWindow();window.addEventListener('resize',handleWindow);function initMap(){var point={lat:59.918326,lng:30.346413,};var map=new google.maps.Map(document.getElementById('map'),{zoom:17,center:point},);var marker=new google.maps.Marker({position:point,map:map,})}
+$(function(){$(document).on('click','a[href^="#"]',function(event){event.preventDefault();$('html, body').animate({scrollTop:$($.attr(this,'href')).offset().top,},500)})})
