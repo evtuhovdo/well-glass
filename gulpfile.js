151123414b979abf593c88f7cd4b4faaa6045a96
Denis Evtukhov <evtuhovdo@gmail..com>
2019-10-29 11:28:04 +0700

    build -> docs

--- gulpfile.js
+++ gulpfile.js
@@ -8,29 +8,29 @@ var clean = require('gulp-clean');
 var del = require('del');
 
 gulp.task('clean', function () {
-  del.sync(['./build/**']);
+  del.sync(['./docs/**']);
 });
 
 gulp.task('copy-main-files', function () {
   return gulp.src(['public/**/*', '!public/css/*.css', '!public/fonts/*', '!public/img/*'])
-    .pipe(gulp.dest('build/'));
+    .pipe(gulp.dest('docs/'));
 });
 
 gulp.task('make-css', function () {
   return gulp.src('public/css/*.css')
     .pipe(postcss([autoprefixer()]))
     .pipe(cleanCSS({ compatibility: 'ie9' }))
-    .pipe(gulp.dest('build/css/'));
+    .pipe(gulp.dest('docs/css/'));
 });
 
 gulp.task('copy-fonts', function () {
   return gulp.src(['public/fonts/*'])
-    .pipe(gulp.dest('build/fonts/'));
+    .pipe(gulp.dest('docs/fonts/'));
 });
 
 gulp.task('copy-images', function () {
   return gulp.src(['public/img/**/*'])
-    .pipe(gulp.dest('build/img/'));
+    .pipe(gulp.dest('docs/img/'));
 });
 
 gulp.task('build', ['clean', 'copy-main-files', 'make-css', 'copy-fonts', 'copy-images'], function () {
